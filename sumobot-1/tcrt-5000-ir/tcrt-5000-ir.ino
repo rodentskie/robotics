b97079776d12e95132ccc8958253c590a82434c4
rodentskie <rodentskie@gmail.com>
2026-09-01 15:45:13 +0800

    feat: ir tracker sensor v2.1

--- sumobot-1/tcrt-5000-ir/tcrt-5000-ir.ino
+++ sumobot-1/tcrt-5000-ir/tcrt-5000-ir.ino
@@ -1,25 +1,31 @@
+// digital pins
 const int rightSensorPin = 8;
 const int leftSensorPin = 7;
+const int centerSensorPin = 6;
 
 void setup() {
   pinMode(rightSensorPin, INPUT);
   pinMode(leftSensorPin, INPUT);
+  pinMode(centerSensorPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   int rightSensorValue = digitalRead(rightSensorPin);
   int leftSensorValue = digitalRead(leftSensorPin);
+  int centerSensorValue = digitalRead(centerSensorPin);
+
   Serial.print("left: ");
   Serial.println(leftSensorValue);
 
   Serial.print("right: ");
   Serial.println(rightSensorValue);
 
+  Serial.print("center: ");
+  Serial.println(centerSensorValue);
 
-  if (rightSensorValue == 0 || leftSensorValue == 0) {
+  if (rightSensorValue == 0 || leftSensorValue == 0 || centerSensorValue == 0) {
     Serial.println("No Object / Black Surface");
-
   } else {
     Serial.println("Object / White Surface Detected");
   }
